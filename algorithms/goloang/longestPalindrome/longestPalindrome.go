b652148406d1dbd363203f02cc46549c72bb9270
Lister Lee <641174410@qq.com>
2021-03-23 16:46:04 +0800

    add 2nd solution 005

--- algorithms/goloang/longestPalindrome/longestPalindrome.go
+++ algorithms/goloang/longestPalindrome/longestPalindrome.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func longestPalindrome(s string) string {
 	ans := ""
@@ -32,10 +34,39 @@ func longestPalindrome(s string) string {
 	return ans
 }
 
+func longestPalindrome1(s string) string {
+	ans := ""
+	start, end := 0, 0
+	n := len(s)
+	for i := 0; i < n; i++ {
+		left1, right1 := expendAroundCenter(i, i, s)
+		left2, right2 := expendAroundCenter(i, i+1, s)
+
+		if right1-left1 > end-start {
+			start = left1
+			end = right1
+		}
+
+		if right2-left2 > end-start {
+			start = left2
+			end = right2
+		}
+	}
+
+	ans = s[start : end+1]
+
+	return ans
+}
+
+func expendAroundCenter(left, right int, s string) (int, int) {
+	for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+	}
+	return left + 1, right - 1
+}
 func main() {
 	str := "babad"
 
-	ans := longestPalindrome(str)
+	ans := longestPalindrome1(str)
 
 	fmt.Println(ans)
 }
