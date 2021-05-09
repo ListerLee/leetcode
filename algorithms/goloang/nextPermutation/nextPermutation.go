065dfd97566679a1cd4949ce5f7cd0ed14759c95
李诗章 <lishizhang@lishizhangdeMacBook-Pro.local>
2021-05-09 17:15:08 +0800

    031 modify

--- algorithms/goloang/nextPermutation/nextPermutation.go
+++ algorithms/goloang/nextPermutation/nextPermutation.go
@@ -2,32 +2,24 @@ package nextPermutation
 
 func nextPermutation(nums []int) {
 	n := len(nums)
-	l := 0
-	for i := n - 1; i > 0; i-- {
-		if nums[i] > nums[i-1] {
-			l = i
-			break
-		}
-	}
-	if l == 0 {
-		reverse(nums, 0, n-1)
-		return
+	l := n - 2
+	for l >= 0 && nums[l+1] <= nums[l] {
+		l--
 	}
 	r := n - 1
-	for i := l; i < n; i++ {
-		if nums[i] <= nums[l-1] {
-			r = i - 1
-			break
+	if l >= 0 {
+		for r >= l && nums[r] <= nums[l] {
+			r--
 		}
+		nums[r], nums[l] = nums[l], nums[r]
 	}
-	nums[l-1], nums[r] = nums[r], nums[l-1]
-	reverse(nums, l, n-1)
+	reverse(nums[l+1:])
 	return
 }
 
-func reverse(nums []int, l, r int) {
-	n := r - l + 1
+func reverse(nums []int) {
+	n := len(nums)
 	for i := 0; i < n/2; i++ {
-		nums[l+i], nums[r-i] = nums[r-i], nums[l+i]
+		nums[i], nums[n-1-i] = nums[n-1-i], nums[i]
 	}
 }
