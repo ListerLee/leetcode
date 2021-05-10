639d94e5b96b1e0c11b82783248bc429d16635f1
李诗章 <lishizhang@lishizhangdeMacBook-Pro.local>
2021-05-10 21:41:15 +0800

    add mergeIntervals 056

---
+++ algorithms/goloang/mergeIntervals/mergeIntervals.go
@@ -0,0 +1,33 @@
+package mergeintervals
+
+import "sort"
+
+type byStart [][]int
+
+func (b byStart) Len() int           { return len(b) }
+func (b byStart) Less(i, j int) bool { return b[i][0] < b[j][0] }
+func (b byStart) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+
+func merge(intervals [][]int) [][]int {
+	n := len(intervals)
+	merge := [][]int{}
+	sort.Sort(byStart(intervals))
+
+	merge = append(merge, intervals[0])
+	for i := 1; i < n; i++ {
+		if merge[len(merge)-1][1] < intervals[i][0] {
+			merge = append(merge, intervals[i])
+		} else {
+			merge[len(merge)-1][1] = max(merge[len(merge)-1][1], intervals[i][1])
+		}
+	}
+	return merge
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
